6513817537428076d7bd9a0ca6477d8815ab37f4
Nicholashuang11 <114864353+Nicholashuang11@users.noreply.github.com>
2026-05-12 21:18:58 +0700

    Update refresh_views.py

--- debootcampzip/refresh_views.py
+++ debootcampzip/refresh_views.py
@@ -11,7 +11,7 @@ logger = logging.getLogger("refresh_views")
 
 DB_CONFIG = {
     "host":     os.getenv("POSTGRES_HOST",     "localhost"),
-    "port":     int(os.getenv("POSTGRES_PORT", "5475")),
+    "port":     int(os.getenv("POSTGRES_PORT", "5432")),
     "dbname":   os.getenv("POSTGRES_DB",       "ecommerce_dw"),
     "user":     os.getenv("POSTGRES_USER",     "ecommerce"),
     "password": os.getenv("POSTGRES_PASSWORD", "ecommerce"),
